23b4293d93b5d6757791c0cfd26646bb21530e3a
Leino <leinovladimir@gmail.com>
2023-12-07 18:44:13 +0400

    loader css for main page nav bar  footer

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://storage.yandexcloud.net/master-strategy/mainPage.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
