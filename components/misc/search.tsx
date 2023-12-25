2517e1a57043f34be30b50fa989f280d1fbdd467
Leino <leinovladimir@gmail.com>
2023-12-25 12:39:18 +0400

    debounce search

--- components/misc/search.tsx
+++ components/misc/search.tsx
@@ -1,8 +1,7 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import PostPreview from '../blog/post-preview';
 import { useRouter } from 'next/router';
 import * as Icon from 'react-bootstrap-icons';
-
 
 function useOutsideAlerter(ref, callback) {
   useEffect(() => {
@@ -27,7 +26,22 @@ function Search({ visible, setVisible }) {
   const router = useRouter();
   const inputRef = useRef(null);
   const containerRef = useRef(null);
+  const [searchQuery, setSearchQuery] = useState(''); // Состояние для хранения запроса
+
+  const [isDebouncing, setIsDebouncing] = useState(false);
+
   const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const debounce = (func, delay) => {
+    let inDebounce;
+    return function () {
+      const context = this;
+      const args = arguments;
+      clearTimeout(inDebounce);
+      inDebounce = setTimeout(() => func.apply(context, args), delay);
+    };
+  };
 
   useEffect(() => {
     if (visible) {
@@ -60,9 +74,26 @@ function Search({ visible, setVisible }) {
   }, [router.asPath]);
 
   async function handleChangeInput(e) {
-    const res = await fetch(`/api/search?q=${e.target.value}`);
+    const value = e.target.value;
+    setSearchQuery(value); // Обновление состояния с текущим значением поля ввода
+
+    if (value.trim() === '') {
+      setSearchResults([]); // Очистка результатов поиска, если поле ввода пустое
+      setIsLoading(false); // Отмена индикатора загрузки
+      return;
+    }
+
+    setIsLoading(true); // Активация индикатора загрузки только если поле ввода не пустое
+    setIsDebouncing(true);
+    const res = await fetch(`/api/search?q=${value}`);
     setSearchResults(await res.json());
+    setIsLoading(false); // Деактивация индикатора загрузки после получения результатов
+    setIsDebouncing(false);
   }
+  const debouncedHandleChangeInput = useCallback(
+    debounce((e) => handleChangeInput(e), 500),
+    []
+  );
 
   return (
     <div
@@ -87,7 +118,7 @@ function Search({ visible, setVisible }) {
               type="search"
               className="form-control w-10 px-2 py-1"
               placeholder="поиск по заметкам..."
-              onChange={handleChangeInput}
+              onChange={debouncedHandleChangeInput}
             />
           </div>
           <button
@@ -97,18 +128,31 @@ function Search({ visible, setVisible }) {
             Вернуться к чтению <Icon.ArrowReturnLeft />
           </button>
         </div>
+        {(isLoading || isDebouncing) && searchQuery && (
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        )}
+
+        {!isLoading && searchQuery && searchResults.length === 0 && (
+          <div className="text-muted">
+            Ничего не найдено. Попробуйте изменить запрос или
+            использовать другие ключевые слова.
+          </div>
+        )}
 
         {/* Search Results */}
-        {searchResults.map((res) => (
-          <PostPreview
-            key={res.item.slug}
-            title={res.item.title}
-            excerpt={res.item.excerpt}
-            slug={res.item.slug}
-            date={res.item.date}
-            author={res.item.author}
-          />
-        ))}
+        {!isLoading &&
+          searchResults.map((res) => (
+            <PostPreview
+              key={res.item.slug}
+              title={res.item.title}
+              excerpt={res.item.excerpt}
+              slug={res.item.slug}
+              date={res.item.date}
+              author={res.item.author}
+            />
+          ))}
       </div>
     </div>
   );
